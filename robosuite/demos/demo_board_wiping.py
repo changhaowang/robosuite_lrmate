fb1fa6a9420934548221b6510d2c4243b8802a96
changhaowang <changhaowang@berkeley.edu>
2023-01-20 16:58:42 -0800

    update script

--- robosuite/demos/demo_board_wiping.py
+++ robosuite/demos/demo_board_wiping.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     options["env_name"] = "Wipe"
     options["robots"] = "LRmate"
     
-    controller_name = "OSC_POSE"
+    controller_name = "OSC_ADM"
 
     # Load the desired controller
     options["controller_configs"] = suite.load_controller_config(default_controller=controller_name)
@@ -20,8 +20,6 @@ if __name__ == "__main__":
     # options["controller_configs"]["I_admittance"] = [50000, 50000, 50000],
     # options["controller_configs"]["kd_admittance"] = [0, 0, 0, 0, 0, 0]
     # options["controller_configs"]["kp_admittance"] = [1000000, 1000000, 4, 10000, 10000, 100000]
-    options["controller_configs"]["control_delta"] = True
-    # options["controller_configs"]["control_ori"] = False
 
     # initialize the task
     env = suite.make(
@@ -37,23 +35,22 @@ if __name__ == "__main__":
     if not render_options["headless"]:
         env.viewer.set_camera(camera_id=0)
 
-    action_dim = env.robots[0].action_dim + env.robots[0].gripper.dof
-    desired_ee_pos = np.array([0.065, -0.012, 1.5])
+    desired_ee_pos = np.array([0.065, -0.012, 0.85])
     desired_ee_ori_mat = env.robots[0].controller.ee_ori_mat
     desired_ee_ori_euler = T.mat2euler(desired_ee_ori_mat)#np.array([-np.pi, 0, np.pi/2])
 
     while True:
         action = np.zeros((6,))
         # Push downwards
-        action[:3] = desired_ee_pos
-        action[3:6] = desired_ee_ori_euler
+        curr_ee_pos = env.robots[0].controller.ee_pos
+        action[:3] = (desired_ee_pos - curr_ee_pos)
         
         env.step(action)
         if not render_options["headless"]:
             env.render()
         sensed_force = env.robots[0].controller.ee_force
-        # calibrated_force = env.robots[0].controller.calibrate_force_sensor_measurement(sensed_force)
-        # # print('Sensed Calibrated Effector Force: ', calibrated_force[-1])
+        calibrated_force = env.robots[0].controller.calibrate_force_sensor_measurement(sensed_force)
+        print('Sensed Calibrated Effector Force: ', calibrated_force[-1])
         # qpos = env.robots[0]._joint_positions
         # qvel = env.robots[0]._joint_velocities
         # ee_pos = env.robots[0].controller.ee_pos
